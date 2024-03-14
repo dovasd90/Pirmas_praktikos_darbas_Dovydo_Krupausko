50acf3bd70a04b90c0bb53cb36f640f62897a1be
Dovydas Krupauskas <dovydas@Dovydass-MacBook-Pro.local>
2024-03-14 20:38:31 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pirmas_praktikos_darbas_Dovydo_Krupausko"
+include(":app")
